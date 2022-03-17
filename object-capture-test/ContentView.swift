64464506aca96fb97c32200e6937f7a85ce0b4eb
Sabrina Bea <me@sabrinabea.com>
2022-03-17 12:08:16 -0400

    Initial Commit

---
+++ object-capture-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  object-capture-test
+//
+//  Created by Sabrina Bea on 3/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ object-capture-test/object_capture_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  object_capture_testApp.swift
+//  object-capture-test
+//
+//  Created by Sabrina Bea on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct object_capture_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
